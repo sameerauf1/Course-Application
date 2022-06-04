ec1e56e1c6da6112f1787b915191880f68712532
Samee Rauf <48734230+sameerauf1@users.noreply.github.com>
2022-06-04 14:57:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-
+import markdown
 from http.server import HTTPServer,BaseHTTPRequestHandler
+
 
 
 class Serv(BaseHTTPRequestHandler): # class that hold server
@@ -7,11 +8,20 @@ class Serv(BaseHTTPRequestHandler): # class that hold server
     def do_GET(self):  # check path of get requeset
         if self.path == '/':   #
             self.path = '/helloworld.html'  # path to index page
-
         if self.path == '/potato':
             self.send_response(200)
             self.end_headers()
-            resp.wfile.write(b'potato potato')
+            self.wfile.write(' b potato potato ' .encode())
+            self.wfile.write(' this messsage was encoded in binary '.encode())
+        if self.path == '/output':  #
+            # self.wfile.write('testing output' .encode())
+            self.path = '/testoutput.html'  # path to index page
+        if self.path == '/.md':   #
+            self.path = '/test.md'
+            markdown.markdownFromFile(input=self.path[1:], output=self.wfile) # convert .md file to html
+            self.wfile.write('this the .md file' .encode())
+              # path to index page
+
         else:
             try:
                 file_to_open = open(self.path[1:]).read()   # .read to load text and read it
