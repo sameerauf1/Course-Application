3a9cb016e45467b1f718e6346cf75703ebdbdfe1
Andy Clifton <andyclifton@gmail.com>
2022-06-15 13:40:21 -0700

    oved some things around in the server:
      *  Index page is `helloworld.html` (same as before)
    
      *  Paths ending in `.md` go through the Markdown translation.
    
      *  Paths ending in `.quiz` also go through the Markdown translation
         (eventually: the quiz-specific translation)
    
      *  Paths ending in `.html` or starting with `/res/` are not
         processed at all. This allows us to place resource files (images,
         scripts, CSS, etc.) in /res/* and serve them as-is.

--- main.py
+++ main.py
@@ -6,31 +6,49 @@ from http.server import HTTPServer,BaseHTTPRequestHandler
 class Serv(BaseHTTPRequestHandler): # class that hold server
 
     def do_GET(self):  # check path of get requeset
+
+        # Rewrite some paths
         if self.path == '/':   #
             self.path = '/helloworld.html'  # path to index page
-        if self.path == '/potato':
-            self.send_response(200)
+
+        if self.path.endswith(".md"):
+            # Plain Markdown, translate to HTML and send
+            try:
+                file_to_open = open(self.path[1:]).read()
+                self.send_response(200)
+            except:
+                file_to_open = "File not found"
+                self.send_response(404)
             self.end_headers()
-            self.wfile.write(' b potato potato ' .encode())
-            self.wfile.write(' this messsage was encoded in binary '.encode())
-        if self.path == '/output':  #
-            # self.wfile.write('testing output' .encode())
-            self.path = '/testoutput.html'  # path to index page
-        if self.path == '/.md':   #
-            self.path = '/test.md'
-            markdown.markdownFromFile(input=self.path[1:], output=self.wfile) # convert .md file to html?
-            self.wfile.write('this the .md file' .encode())
-              # path to index page
+            markdown.markdownFromFile(input=self.path[1:], output=self.wfile)
+
+        elif self.path.endswith(".quiz"):
+            # Quiz Markdown, translate to HTML form and send
+            try:
+                file_to_open = open(self.path[1:]).read()
+                self.send_response(200)
+            except:
+                file_to_open = "File not found"
+                self.send_response(404)
+            self.end_headers()
+            markdown.markdownFromFile(input=self.path[1:], output=self.wfile)
+
+        elif self.path.endswith(".html") or self.path.startswith("/res/"):
+            # Plain HTML, send as is
+            try:
+                file_to_open = open(self.path[1:]).read()  
+                self.send_response(200)  
+            except:
+                file_to_open = "File not found"  
+                self.send_response(404)  
+            self.end_headers()
+            self.wfile.write(bytes(file_to_open, 'utf-8'))  
 
         else:
-            try:
-                file_to_open = open(self.path[1:]).read()   # .read to load text and read it
-                self.send_response(200)   # 200 response if opening the webpage is sucessful
-            except:
-                file_to_open = "File not found"   # display file no tfound
-                self.send_response(404)   # sent a 404 response that it was unsucessful
-            self.end_headers()
-            self.wfile.write(bytes(file_to_open, 'utf-8'))   # write contents of file to screen by converting to bytes
+            self.send_response(404)
+            self.end_headers()                         
+        
+
 
 
 httpd = HTTPServer(('localhost', 8080), Serv)   # httpd a program that runs in the background, local host
